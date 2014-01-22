b445af679986fc6a7962ae5be8c27c5a5ad44b87
fabricio2501 <fegrijalva2501@hotmail.com>
2014-01-22 12:53:33 -0500

    dominio TipoTransaccion

---
+++ grails-app/domain/cratos/sri/TipoTransaccion.groovy
@@ -0,0 +1,22 @@
+package cratos
+class TipoTransaccion implements Serializable {
+
+    String descripcion
+    String codigo
+
+    static mapping = {
+        table 'tptr'
+        cache usage: 'read-write', include: 'non-lazy'
+        id column: 'tptr__id'
+        id generator: 'identity'
+        version false
+        columns {
+            codigo column: 'tptrcdgo'
+            descripcion column: 'tptrdscr'
+        }
+    }
+    static constraints = {
+        codigo(size: 1..1, blank: false, attributes: [title: 'codigo'])
+        descripcion(blank: false, maxSize: 31, attributes: [title: 'descripcion'])
+    }
+}
